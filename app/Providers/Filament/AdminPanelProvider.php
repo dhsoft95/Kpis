f24f5bb604351967c97cba77589ca630dd633bfe
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-10 16:31:27 +0300

    dfghj

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,7 +50,8 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => '#fcbb29',
                 'secondary' => '#fcbb29',
-            ]) ->defaultThemeMode(ThemeMode::Dark)  ->sidebarFullyCollapsibleOnDesktop()->topNavigation()
+            ]) ->defaultThemeMode(ThemeMode::Dark)  ->sidebarFullyCollapsibleOnDesktop()
+//            ->topNavigation()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
 //            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([customer::class,financial::class,oparations::class,service::class])
