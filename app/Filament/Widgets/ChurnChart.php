7fb10ab74a3f01bcca5e873d15d054175a7b1b91
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-22 22:59:17 +0300

    db connect

--- app/Filament/Widgets/ChurnChart.php
+++ app/Filament/Widgets/ChurnChart.php
@@ -75,7 +75,7 @@ class ChurnChart extends ChartWidget
     {
         $dateFormat = $this->getSqlDateFormat($groupBy);
 
-        $data = DB::table('users')
+        $data = DB::connection('mysql_second')->table('users')
             ->leftJoin('tbl_transactions', 'users.phone_number', '=', 'tbl_transactions.sender_phone')
             ->whereNull('tbl_transactions.sender_phone')
             ->whereBetween('users.created_at', [$start, $end])
