465ec296f3db48e96a9b1aa2a5ecfb5807c9b24d
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-23 23:42:41 +0300

    chja

--- resources/views/filament/widgets/user-stats-overview.blade.php
+++ resources/views/filament/widgets/user-stats-overview.blade.php
@@ -5,6 +5,7 @@
 
         <!-- Include Font Awesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+
         <style>
             /* Progress bar styles and animations */
             .progress-bar {
