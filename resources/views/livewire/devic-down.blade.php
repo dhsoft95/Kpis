6863ebc8f281816436c194d5bb7bd61090f0e98c
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-23 21:36:09 +0300

    jjjs

--- resources/views/livewire/devic-down.blade.php
+++ resources/views/livewire/devic-down.blade.php
@@ -1,6 +1,6 @@
 <x-filament::widget>
     <x-filament::card>
-        <div class="space-y-4">
+        <div class="space-y-6">
             <!-- Device Downloads Section -->
             <div>
                 <h3 class="text-sm font-medium text-gray-900 dark:text-white mb-2">Device Downloads</h3>
