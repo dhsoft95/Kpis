570e1888e8b0eee20495a928e39d68c110b0cc9e
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-23 12:55:29 +0300

    chha

--- app/Filament/Widgets/RegisterdUsersChart.php
+++ app/Filament/Widgets/RegisterdUsersChart.php
@@ -20,7 +20,7 @@ class RegisterdUsersChart extends ApexChartWidget
 
 //    protected int | string | array $columnSpan = 'full';
 //    protected static ?int $contentHeight = 440;
-    protected static ?int $contentHeight = 250;
+    protected static ?int $contentHeight = 200;
 
 
 
