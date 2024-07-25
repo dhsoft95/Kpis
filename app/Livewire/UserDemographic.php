e54225195c0888594888f1e8fd62cc74704545a5
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-25 15:38:12 +0300

    Teat

--- app/Livewire/UserDemographic.php
+++ app/Livewire/UserDemographic.php
@@ -3,30 +3,36 @@
 namespace App\Livewire;
 
 use Filament\Widgets\Widget;
+use Livewire\WithPagination;
 
 class UserDemographic extends Widget
 {
-    public function getViewData(): array
+    protected static string $view = 'livewire.user-demographic';
+
+    use WithPagination;
+
+    public $topCountries = [];
+    public $countryData = [];
+
+    public function mount()
     {
-        return [
-            'countryData' => [
-                ['Country', 'Popularity'],
-                ['Germany', 200],
-                ['United States', 300],
-                ['Brazil', 400],
-                ['Canada', 500],
-                ['France', 600],
-                ['RU', 700]
-            ],
-            'topCountries' => [
-                ['name' => 'United States', 'code' => 'us', 'percentage' => '35%'],
-                ['name' => 'Canada', 'code' => 'ca', 'percentage' => '26%'],
-                ['name' => 'France', 'code' => 'fr', 'percentage' => '18%'],
-                ['name' => 'Italy', 'code' => 'it', 'percentage' => '14%'],
-                ['name' => 'Australia', 'code' => 'au', 'percentage' => '10%'],
-                ['name' => 'India', 'code' => 'in', 'percentage' => '7%'],
-            ],
+        $this->topCountries = [
+            ['name' => 'United States', 'code' => 'us', 'percentage' => '35%'],
+            ['name' => 'Canada', 'code' => 'ca', 'percentage' => '26%'],
+            ['name' => 'France', 'code' => 'fr', 'percentage' => '18%'],
+            ['name' => 'Italy', 'code' => 'it', 'percentage' => '14%'],
+            ['name' => 'Australia', 'code' => 'au', 'percentage' => '10%'],
+            ['name' => 'India', 'code' => 'in', 'percentage' => '7%'],
+        ];
+
+        $this->countryData = [
+            ['Country', 'Popularity'],
+            ['Germany', 200],
+            ['United States', 300],
+            ['Brazil', 400],
+            ['Canada', 500],
+            ['France', 600],
+            ['RU', 700]
         ];
     }
-    protected static string $view = 'livewire.user-demographic';
 }
