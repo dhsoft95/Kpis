e5675356cc608a20389b54e3999df5821106a7f0
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-25 22:07:05 +0300

    dd

--- resources/views/livewire/user-demographic.blade.php
+++ resources/views/livewire/user-demographic.blade.php
@@ -1,31 +1,37 @@
-<div wire:init="renderChart">
-    <div id="geo_chart" style="width: 100%; height: 500px;"></div>
-</div>
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <html>
+        <head>
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script type="text/javascript">
+                google.charts.load('current', {
+                    'packages':['geochart'],
+                });
+                google.charts.setOnLoadCallback(drawRegionsMap);
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
-    google.charts.load('current', {
-        'packages':['geochart'],
-        'mapsApiKey': 'YOUR_GOOGLE_MAPS_API_KEY'
-    });
-    google.charts.setOnLoadCallback(drawRegionsMap);
+                function drawRegionsMap() {
+                    var data = google.visualization.arrayToDataTable([
+                        ['Country', 'Popularity'],
+                        ['Germany', 200],
+                        ['United States', 300],
+                        ['Brazil', 400],
+                        ['Canada', 500],
+                        ['France', 600],
+                        ['RU', 700]
+                    ]);
 
-    function drawRegionsMap() {
-        var data = google.visualization.arrayToDataTable([
-            ['Country', 'Popularity'],
-            ['Germany', 200],
-            ['United States', 300],
-            ['Brazil', 400],
-            ['Canada', 500],
-            ['France', 600],
-            ['RU', 700]
-        ]);
+                    var options = {};
 
-        var options = {};
+                    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
-        var chart = new google.visualization.GeoChart(document.getElementById('geo_chart'));
+                    chart.draw(data, options);
+                }
+            </script>
+        </head>
+        <body>
+        <div id="regions_div" style="width: 900px; height: 500px;"></div>
+        </body>
+        </html>
 
-        chart.draw(data, options);
-    }
-</script>
-
+    </x-filament::section>
+</x-filament-widgets::widget>
