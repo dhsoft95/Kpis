b12f57a3ef40be49da1587a3f3f89967e782e300
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-25 21:56:47 +0300

    test

--- resources/views/livewire/user-demographic.blade.php
+++ resources/views/livewire/user-demographic.blade.php
@@ -1,6 +1,6 @@
 <x-filament::widget>
     <x-filament::card>
-        <div id="geo-chart" style="width: 100%; height: 500px;"></div>
+        <div id="geo-chart" style="width: 100%; height: 400px;"></div>
     </x-filament::card>
 
     @pushOnce('scripts')
@@ -8,17 +8,23 @@
     @endPushOnce
 
     <script>
+        console.log('Widget script started');
         document.addEventListener('livewire:load', function () {
+            console.log('Livewire loaded');
             google.charts.load('current', {
                 'packages': ['geochart']
             });
             google.charts.setOnLoadCallback(drawRegionsMap);
 
             function drawRegionsMap() {
+                console.log('Drawing map');
                 var data = google.visualization.arrayToDataTable(@json($chartData));
                 var options = @json($chartOptions);
+                console.log('Chart data:', data);
+                console.log('Chart options:', options);
                 var chart = new google.visualization.GeoChart(document.getElementById('geo-chart'));
                 chart.draw(data, options);
+                console.log('Map drawn');
             }
 
             window.addEventListener('resize', drawRegionsMap);
