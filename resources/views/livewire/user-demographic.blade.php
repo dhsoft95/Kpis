d4ca335e3dd10a3d68ec2f120fbb36f531aed863
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-07-25 22:05:43 +0300

    rtest

--- resources/views/livewire/user-demographic.blade.php
+++ resources/views/livewire/user-demographic.blade.php
@@ -1,33 +1,31 @@
-<x-filament::widget>
-    <x-filament::card>
-        <div id="geo-chart" style="width: 100%; height: 400px;"></div>
-    </x-filament::card>
+<div wire:init="renderChart">
+    <div id="geo_chart" style="width: 100%; height: 500px;"></div>
+</div>
 
-    @pushOnce('scripts')
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    @endPushOnce
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+    google.charts.load('current', {
+        'packages':['geochart'],
+        'mapsApiKey': 'YOUR_GOOGLE_MAPS_API_KEY'
+    });
+    google.charts.setOnLoadCallback(drawRegionsMap);
 
-    <script>
-        console.log('Widget script started');
-        document.addEventListener('livewire:load', function () {
-            console.log('Livewire loaded');
-            google.charts.load('current', {
-                'packages': ['geochart']
-            });
-            google.charts.setOnLoadCallback(drawRegionsMap);
+    function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable([
+            ['Country', 'Popularity'],
+            ['Germany', 200],
+            ['United States', 300],
+            ['Brazil', 400],
+            ['Canada', 500],
+            ['France', 600],
+            ['RU', 700]
+        ]);
 
-            function drawRegionsMap() {
-                console.log('Drawing map');
-                var data = google.visualization.arrayToDataTable(@json($chartData));
-                var options = @json($chartOptions);
-                console.log('Chart data:', data);
-                console.log('Chart options:', options);
-                var chart = new google.visualization.GeoChart(document.getElementById('geo-chart'));
-                chart.draw(data, options);
-                console.log('Map drawn');
-            }
+        var options = {};
 
-            window.addEventListener('resize', drawRegionsMap);
-        });
-    </script>
-</x-filament::widget>
+        var chart = new google.visualization.GeoChart(document.getElementById('geo_chart'));
+
+        chart.draw(data, options);
+    }
+</script>
+
